4a80c515c2d245fe8fd96d6b8ea74d3fc8a80c15
beniaminPC <beniamin_s1@yahoo.com>
2017-01-01 02:13:53 +0200

    initial commit

---
+++ UpComingGibs/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UpComingGibs.Startup))]
+namespace UpComingGibs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
