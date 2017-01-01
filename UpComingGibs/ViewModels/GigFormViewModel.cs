17db41e0be63c7dc487d517e6b9179c961d123c8
beniaminPC <beniamin_s1@yahoo.com>
2017-01-01 03:50:46 +0200

    changes

---
+++ UpComingGibs/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using UpComingGibs.Models;
+
+namespace UpComingGibs.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public int Genre { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
